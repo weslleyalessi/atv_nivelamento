9e9f535ca65c595ba29b618422acaf1fdc88e344
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-03-07 08:40:41 -0300

    ok

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes)
+
+for(let i=0; i <  botões.length; i++){
+    console.log(i);
+}
+
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add(“ativo”);
+}
+
+for(let j=0;j<botoes.length;j++){
+    botoes[j].classList.remove("ativo");
+}
+
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
+
