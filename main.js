eed580a84d15e3996319b6d3565cb819f16d7c83
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-03-21 08:00:16 -0300

    ok

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba-conteudo");
 
 for(let i=0; i <botoes.lenth; i++){
     botoes[i].onclick = function(){
-        for(let i=0; j=0<botoes.lenth;j++){
+        for(let j=0; j=0<botoes.lenth;j++){
             botoes[j].classList.remove("ativo");
-
+            textos[j].classList.remove("ativo");
         }
         botoes[1].classList.add("ativo");
+        textos[1].classList.add("ativo");
+
     }
 }
