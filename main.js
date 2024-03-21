595879aabe764fdd93a4df1cf89d5a03badbf815
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-03-21 08:01:29 -0300

    ok

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll("aba-conteudo");
 
-for(let i=0; i <botoes.lenth; i++){
+for(let i=0; i <botoes.length; i++){
     botoes[i].onclick = function(){
-        for(let j=0; j=0<botoes.lenth;j++){
+        for(let j=0; j=0<botoes.length;j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
