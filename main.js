d336123d1ef5066cc42f4db445cdad193ef56de2
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-03-21 07:54:43 -0300

    ok

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
+
+for(let i=0; i <botoes.lenth; i++){
+    botoes[i].onclick = function(){
+        for(let i=0; j=0<botoes.lenth;j++){
+            botoes[j].classList.remove("ativo");
+
+        }
+        botoes[1].classList.add("ativo");
+    }
+}
