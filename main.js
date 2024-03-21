601ed14501d3873d5b322b18490fd1b63089b91b
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-03-21 08:44:49 -0300

    ok

--- main.js
+++ main.js
@@ -14,4 +14,5 @@ for(let i=0; i<botoes.length; i++){
     }
 }
 const tempoObjetivo1 = new Date("2024-03-21T00:00:00");
-contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
