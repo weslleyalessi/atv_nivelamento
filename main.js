98bceb856dcc646fbca6715399a57645fbb948b7
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-04-11 08:17:16 -0300

    ok

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
 
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
