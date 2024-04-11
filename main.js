b3aa006d593a9fec105e514968516bfe02b9674b
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-04-11 08:27:10 -0300

    ok

--- main.js
+++ main.js
@@ -16,10 +16,12 @@ for(let i=0; i<botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-09-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-08-05T00:00:00");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempo = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -32,6 +34,11 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+
+    if(tempoFinal > 0){ 
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    }else{
+        return "Prazo encerrado";
+    }
  
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
