54d885a11a2b87a16586073262a72bcd8fffd9ce
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2024-04-11 08:10:44 -0300

    ok

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ const contadores = document.querySelectorAll(".contador");
 
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
+        
         for(let j=0; j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
+       
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
 
     }
 }
-const tempoObjetivo1 = new Date("2024-03-21T00:00:00");
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
+
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
